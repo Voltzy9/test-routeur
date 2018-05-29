9fd5e2f64f591c455a0703c82fca0b6a5169bd9b
tentacode <contact@gabrielpillet.com>
2018-05-29 16:39:26 +0200

    Ajout commentaires

--- public/index.php
+++ public/index.php
@@ -1,14 +1,24 @@
 <?php
 
+// on récupère l'adresse après l'host du site
+// exemple : avec http://site/test, $url correspondra à "/test"
 $url = $_SERVER['REQUEST_URI'];
 
+// si on est sur l'accueil
 if ($url == '/') {
     require_once(__DIR__.'/../src/pages/homepage.php');
-} elseif (preg_match('/^\/administration/', $url)) {
+}
+// si l'url commence par /administation
+elseif (preg_match('/^\/administration/', $url)) {
     require_once(__DIR__.'/../src/pages/admin.php');
-} elseif (preg_match('/^\/offer\/([0-9]+)/', $url, $matches)) {
+}
+// si l'url est de la forme /offer/123/quelque-chose
+elseif (preg_match('/^\/offer\/([0-9]+)/', $url, $matches)) {
+    // on récupère l'id de l'offre
     $offerId = $matches[1];
     require_once(__DIR__.'/../src/pages/offer.php');
-} else {
+}
+// dans tout les autres cas on affiche une page d'erreur
+else {
     require_once(__DIR__.'/../src/pages/notfound.php');
 }
